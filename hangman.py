615e66ade7c29547adcd08cd4a8022061ea0d65b
IonutHaita <ionut.haita93@gmail.com>
2024-09-16 22:00:53 +0300

    .

--- hangman.py
+++ hangman.py
@@ -3,7 +3,7 @@ import random
 placeholder = []
 word_list = ["door", "cauliflower", "baboon"]
 game_over = False
-didnt_guess = 0
+didnt_guess = -1
 guesses = 0
 winner = ['''
           \ O /
@@ -87,27 +87,26 @@ while not game_over:
     display = ""
     guess = input("Guess a letter: ").lower()
     print(guess)
-    guessed = False
     index = 0
     for letter in chosen_word:
         if letter == guess:
             guesses += 1
             placeholder[index] = letter
-            guessed = True
         index += 1
 
     for char in placeholder:
         display += char
     print(display)
-    if not guessed:
+
+    if guess not in chosen_word:
         didnt_guess += 1
+        print(stage[didnt_guess])
 
     if "_" not in display:
         game_over = True
         print("Well done!")
         print(winner[0])
-    if not guessed:
-        print(stage[didnt_guess])
+
     if didnt_guess == 0:
         game_over = True
         print("Sorry, you failed to guess the word!\nYour character has been hanged")
